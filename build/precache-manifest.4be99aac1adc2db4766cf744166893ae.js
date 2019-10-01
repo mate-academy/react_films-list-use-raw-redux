1d1d0492e09c53077bde26fe5794d79ba70190ca
igaryok <igor.sobko@gmail.com>
2019-10-01 21:34:45 +0300

    change state serachWord

--- build/precache-manifest.4be99aac1adc2db4766cf744166893ae.js
+++ build/precache-manifest.4be99aac1adc2db4766cf744166893ae.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4531a39fea2d73de1da092ca413aa738",
+    "revision": "cca00c2df524d1ca98013a5d5db7c7f2",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.1ab5750d.chunk.css"
   },
   {
-    "revision": "8dd65b90cd28977a0727",
+    "revision": "754785f25eb5d3b62bb9",
     "url": "./static/css/main.34974d95.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.0142f2df.chunk.js"
   },
   {
-    "revision": "8dd65b90cd28977a0727",
-    "url": "./static/js/main.94f08d4b.chunk.js"
+    "revision": "754785f25eb5d3b62bb9",
+    "url": "./static/js/main.78800774.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e2a1af8659313e9756f5a59e1be28a06.js"
+  "./precache-manifest.4be99aac1adc2db4766cf744166893ae.js"
 );
 
 self.addEventListener('message', (event) => {
