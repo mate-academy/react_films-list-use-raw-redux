a34a42c8e3cbfa3fc666f1e1b50d90b572c921b2
Viktoriia Tsykun <vikatsikun@gmail.com>
2019-10-01 17:15:29 +0300

    Some fixes

--- src/components/FilmDetails/FilmDetails.js
+++ src/components/FilmDetails/FilmDetails.js
@@ -4,7 +4,7 @@ import { store } from '../../store';
 
 export class FilmDetails extends Component {
   state = {
-    film: {},
+    film: store.getState().films.find(film => String(film.id) === this.props.match.params.id),
   }
 
   unsubscribe = null;
@@ -25,12 +25,7 @@ export class FilmDetails extends Component {
 
   render() {
     const {
-      film: {
-        title,
-        description,
-        imgUrl,
-        imdbUrl,
-      },
+      film,
     } = this.state;
 
     return (
@@ -38,7 +33,7 @@ export class FilmDetails extends Component {
         <div className="card-image">
           <figure className="image is-4by3">
             <img
-              src={imgUrl}
+              src={film.imgUrl}
               alt="Film logo"
             />
           </figure>
@@ -54,14 +49,14 @@ export class FilmDetails extends Component {
               </figure>
             </div>
             <div className="media-content">
-              <p className="title is-4">{title}</p>
+              <p className="title is-4">{film.title}</p>
             </div>
           </div>
 
           <div className="content">
-            {description}
+            {film.description}
             <br />
-            <a href={imdbUrl}>IMDB</a>
+            <a href={film.imdbUrl}>IMDB</a>
           </div>
         </div>
       </div>
