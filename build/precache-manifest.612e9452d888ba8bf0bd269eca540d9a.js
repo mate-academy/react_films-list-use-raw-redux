42adb03bd96341eea49d43d0f75af93bca0b8b65
VladyslavSak <iblackvour@gmail.com>
2019-10-04 15:49:00 +0300

    build for deploy

---
+++ build/precache-manifest.612e9452d888ba8bf0bd269eca540d9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32339428e1c7ee1e59f73c2443a2d85d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "576b6d9ebec33aa0e459",
+    "url": "./static/css/main.960b0643.chunk.css"
+  },
+  {
+    "revision": "0487b245391193d797b3",
+    "url": "./static/js/2.382c5d8e.chunk.js"
+  },
+  {
+    "revision": "576b6d9ebec33aa0e459",
+    "url": "./static/js/main.6722dc29.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
