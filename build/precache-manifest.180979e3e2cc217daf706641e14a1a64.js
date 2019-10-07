91e7083c78318e20223fcfcf33024d4e1dc75d91
vladyslav223 <vladyslav223@gmail.com>
2019-10-08 01:21:06 +0300

    working

---
+++ build/precache-manifest.180979e3e2cc217daf706641e14a1a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379987a2312d0e4db5aef70079b26c4e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d8fe01b92c555e55709",
+    "url": "./static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "517d95c2eeb15f9f1c67",
+    "url": "./static/css/main.8ce20dc0.chunk.css"
+  },
+  {
+    "revision": "1d8fe01b92c555e55709",
+    "url": "./static/js/2.e316b7da.chunk.js"
+  },
+  {
+    "revision": "517d95c2eeb15f9f1c67",
+    "url": "./static/js/main.6dbc571d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
