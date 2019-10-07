5fb61e40b174ad49a1b060998558ef09e7141a28
VladyslavSak <iblackvour@gmail.com>
2019-10-07 07:54:21 +0300

    Test

---
+++ build/precache-manifest.8a3522e13614c3a25bdbf39ab769a6f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b10775c67e33fbc57ac57f13c004c78",
+    "url": "./index.html"
+  },
+  {
+    "revision": "14e4b61f13671245c290",
+    "url": "./static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "a76280e389da0b9a8372",
+    "url": "./static/css/main.fc266a5d.chunk.css"
+  },
+  {
+    "revision": "14e4b61f13671245c290",
+    "url": "./static/js/2.7fe3187a.chunk.js"
+  },
+  {
+    "revision": "a76280e389da0b9a8372",
+    "url": "./static/js/main.eff375b7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
