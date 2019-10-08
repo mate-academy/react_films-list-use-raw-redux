c60eda6f0a50f922f83837b7e6f84df09970635a
Khrystyna Styslo <khrystyna.styslo@gmail.com>
2019-10-08 15:37:37 +0300

    update package.json

---
+++ build/precache-manifest.88c24be436ba7f698245ecda3752c9a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7a4965be1c0cfc7c233201413b37da",
+    "url": "/react_films-list-use-raw-redux/index.html"
+  },
+  {
+    "revision": "1ddc4f6e4b2f2e950490",
+    "url": "/react_films-list-use-raw-redux/static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "51e5b83a533e755db113",
+    "url": "/react_films-list-use-raw-redux/static/css/main.793628a1.chunk.css"
+  },
+  {
+    "revision": "1ddc4f6e4b2f2e950490",
+    "url": "/react_films-list-use-raw-redux/static/js/2.8badf651.chunk.js"
+  },
+  {
+    "revision": "51e5b83a533e755db113",
+    "url": "/react_films-list-use-raw-redux/static/js/main.14fb44da.chunk.js"
+  },
+  {
+    "revision": "7588ac357f07406e5d63",
+    "url": "/react_films-list-use-raw-redux/static/js/runtime~main.1e9a4e9f.js"
+  }
+]);
