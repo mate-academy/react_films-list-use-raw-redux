2b6164b09dc8bca4f9c684c6deb937dcaecfbef8
amserhii11 <amelin.serhii@gmail.com>
2019-10-09 16:22:20 +0300

    All tasks

---
+++ build/precache-manifest.0702f13d80f21c1a3644b719aef2653f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c36181004206f793c7f4afdf30b182e",
+    "url": "/react_films-list-use-raw-redux/index.html"
+  },
+  {
+    "revision": "d512b02291a275786cd7",
+    "url": "/react_films-list-use-raw-redux/static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "eb5c056ad1d10506d7f7",
+    "url": "/react_films-list-use-raw-redux/static/css/main.3382c7ad.chunk.css"
+  },
+  {
+    "revision": "d512b02291a275786cd7",
+    "url": "/react_films-list-use-raw-redux/static/js/2.eb31ca6a.chunk.js"
+  },
+  {
+    "revision": "eb5c056ad1d10506d7f7",
+    "url": "/react_films-list-use-raw-redux/static/js/main.feffc3c9.chunk.js"
+  },
+  {
+    "revision": "7588ac357f07406e5d63",
+    "url": "/react_films-list-use-raw-redux/static/js/runtime~main.1e9a4e9f.js"
+  }
+]);
