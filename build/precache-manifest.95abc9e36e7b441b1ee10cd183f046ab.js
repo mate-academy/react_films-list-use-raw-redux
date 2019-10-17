b2cfa6cb5a52a6e5ed4f66e17d79498a89a1d6bc
Rafler <amuronyk@gmail.com>
2019-10-17 11:02:14 +0300

    ...

---
+++ build/precache-manifest.95abc9e36e7b441b1ee10cd183f046ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a502f6b0379924b1bf7c31aa7bf7a70",
+    "url": "/react_films-list-use-raw-redux/index.html"
+  },
+  {
+    "revision": "1ddc4f6e4b2f2e950490",
+    "url": "/react_films-list-use-raw-redux/static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "72c51a189ca26cf50a35",
+    "url": "/react_films-list-use-raw-redux/static/css/main.276d5381.chunk.css"
+  },
+  {
+    "revision": "1ddc4f6e4b2f2e950490",
+    "url": "/react_films-list-use-raw-redux/static/js/2.8badf651.chunk.js"
+  },
+  {
+    "revision": "72c51a189ca26cf50a35",
+    "url": "/react_films-list-use-raw-redux/static/js/main.5464bc09.chunk.js"
+  },
+  {
+    "revision": "7588ac357f07406e5d63",
+    "url": "/react_films-list-use-raw-redux/static/js/runtime~main.1e9a4e9f.js"
+  }
+]);
