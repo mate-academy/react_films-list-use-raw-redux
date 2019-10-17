06718c2beb4dc927109ef57b58c96ed479d87594
Rafler <amuronyk@gmail.com>
2019-10-17 10:57:39 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 import React, { Component } from 'react';
 import './App.scss';
-import { FilmsList } from './components/FilmsList';
-import { NewFilm } from './components/NewFilm';
-import { films } from './data';
-import { FormField } from './components/FormField';
 import {
   BrowserRouter,
   Switch,
   Route,
 } from 'react-router-dom';
+import { FilmsList } from './components/FilmsList';
+import { NewFilm } from './components/NewFilm';
+import { FormField } from './components/FormField';
 import { FilmDetails } from './components/FilmDetails';
+import { store, addNewFilm } from './store';
 
-const API_URL = 'http://www.omdbapi.com/?apikey=2f4a38c9&t=';
+const API_URL = 'https://www.omdbapi.com/?apikey=2f4a38c9&t=';
 
 export class App extends Component {
   state = {
-    filmsList: films,
     searchWord: '',
   };
 
-  componentDidMount() {
-    this.searchFilm('spider');
-  }
-
   handleAddFilm = (newFilm) => {
-    this.setState(prevState => ({
-      filmsList: [
-        ...prevState.filmsList,
-        {
-          id: prevState.filmsList[prevState.filmsList.length - 1].id + 1,
-          ...newFilm,
-        },
-      ],
-    }));
+    store.dispatch(addNewFilm(newFilm));
   };
 
   handleSearchChange = ({ target }) => {
@@ -59,14 +46,12 @@ export class App extends Component {
           imdbUrl: Website,
         };
 
-        this.setState(prevState => ({
-          filmsList: [...prevState.filmsList, newFilm],
-        }));
+        store.dispatch(addNewFilm(newFilm));
       });
   };
 
   render() {
-    const { filmsList, searchWord } = this.state;
+    const { searchWord } = this.state;
 
     return (
       <BrowserRouter>
@@ -93,21 +78,12 @@ export class App extends Component {
               <Route
                 exact
                 path="/"
-                render={() => (
-                  <FilmsList films={filmsList} />
-                )}
+                component={FilmsList}
               />
               <Route
                 exact
                 path="/film/:id"
-                render={({ match }) => {
-                  const film = filmsList
-                    .find(f => String(f.id) === match.params.id);
-
-                  return (
-                    <FilmDetails {...film} />
-                  );
-                }}
+                component={FilmDetails}
               />
             </Switch>
           </div>
